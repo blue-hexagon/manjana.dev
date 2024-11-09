728f8b67aa49a31616323d08ad6c6344cae022eb
blue-hexagon <w0j8uhv5csio@opayq.net>
2024-11-09 19:12:10 +0100

    Fix issue that causes Layout not to be rendered.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { ThemeProvider } from "@mui/material/styles";
+import {ThemeProvider} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from "./src/theme"; // you can customize your theme here
+import theme from "./src/theme";
+import "./src/css/prism-material-dark.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import './src/css/main.css';
+import Layout from "./src/components/Layout";
 
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    {element}
-  </ThemeProvider>
+export const wrapRootElement = ({element}) => (
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        {element}
+    </ThemeProvider>
 );
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>;
+};
